6f1bf87c2a838b2b3f4c12807d0912095a68786e
Robert Strandh <robert.strandh@gmail.com>
2015-12-25 15:10:46 +0100

    Instantiate all possible directions.

--- example-model-instance.lisp
+++ example-model-instance.lisp
@@ -1,1 +1,7 @@
 (cl:in-package #:clim-demo-adventure)
+
+(defparameter *north* (make-instance 'north))
+(defparameter *south* (make-instance 'south))
+(defparameter *east* (make-instance 'east))
+(defparameter *west* (make-instance 'west))
+
