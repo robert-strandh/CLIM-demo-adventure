1bf9622349941e7fd6c7d0ab2c52a5a9b5b3ae9d
Robert Strandh <robert.strandh@gmail.com>
2015-12-26 10:17:22 +0100

    Define function for creating a world.

--- example-model-instance.lisp
+++ example-model-instance.lisp
@@ -25,3 +25,22 @@
 (reinitialize-instance
  *se*
  :directions `((,"west" . ,*sw*) (,"north" . ,*ne*)))
+
+(defun make-world ()
+  (let ((bucket (bucket))
+	(pen (pen))
+	(erasor (erasor))
+	(notebook (notebook)))
+    (let ((nw (make-instance 'place :objects (list bucket pen)))
+	  (ne (make-instance 'place :objects (list erasor)))
+	  (sw (make-instance 'place :objects (list notebook)))
+	  (se (make-instance 'place :objects (list))))
+      (reinitialize-instance
+       nw :directions `((,"east" . ,ne) (,"south" . ,sw)))
+      (reinitialize-instance
+       ne :directions `((,"west" . ,nw) (,"south" . ,se)))
+      (reinitialize-instance
+       sw :directions `((,"east" . ,se) (,"north" . ,nw)))
+      (reinitialize-instance
+       se :directions `((,"west" . ,sw) (,"north" . ,ne)))
+      nw)))
