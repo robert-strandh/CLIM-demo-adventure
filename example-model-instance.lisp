0f1dc0b71662eee19822e462d0904bdad9c4fea0
Robert Strandh <robert.strandh@gmail.com>
2015-12-26 10:19:55 +0100

    Remove special variables that are no longer needed.

--- example-model-instance.lisp
+++ example-model-instance.lisp
@@ -1,31 +1,5 @@
 (cl:in-package #:clim-demo-adventure)
 
-(defparameter *bucket* (bucket))
-(defparameter *pen* (pen))
-(defparameter *erasor* (erasor))
-(defparameter *notebook* (notebook))
-
-(defparameter *nw* (make-instance 'place :objects (list *bucket* *pen*)))
-(defparameter *ne* (make-instance 'place :objects (list *erasor*)))
-(defparameter *sw* (make-instance 'place :objects (list *notebook*)))
-(defparameter *se* (make-instance 'place :objects (list)))
-
-(reinitialize-instance
- *nw*
- :directions `((,"east" . ,*ne*) (,"south" . ,*sw*)))
-
-(reinitialize-instance
- *ne*
- :directions `((,"west" . ,*nw*) (,"south" . ,*se*)))
-
-(reinitialize-instance
- *sw*
- :directions `((,"east" . ,*se*) (,"north" . ,*nw*)))
-
-(reinitialize-instance
- *se*
- :directions `((,"west" . ,*sw*) (,"north" . ,*ne*)))
-
 (defun make-world ()
   (let ((bucket (bucket))
 	(pen (pen))
