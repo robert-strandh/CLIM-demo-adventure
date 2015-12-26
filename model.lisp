e66c5d77c746993eb70bde673219704377604a76
Robert Strandh <robert.strandh@gmail.com>
2015-12-26 08:09:05 +0100

    Remove unused class WOLD.

--- model.lisp
+++ model.lisp
@@ -2,11 +2,6 @@
 
 (defgeneric show (thing stream))
 
-;;; The class that holds an instance of the full model to drive the
-;;; game.
-(defclass world ()
-  ((%places :initarg :places :reader places)))
-
 ;;; This is the base class for all places that can be members of the
 ;;; set of places of the world.
 (defclass place ()
