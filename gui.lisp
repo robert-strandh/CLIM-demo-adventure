385aaf60a71a3966ff7576c650608787c3a552b6
Robert Strandh <robert.strandh@gmail.com>
2015-12-26 07:22:19 +0100

    Create application frame for the game.

--- gui.lisp
+++ gui.lisp
@@ -1,1 +1,12 @@
 (cl:in-package #:clim-demo-adventure-gui)
+
+(define-application-frame adventure ()
+  ((%visited-places :initarg :visited-places :accessor visited-places))
+  (:panes
+   (text :application
+	 :width 800 :height 400
+	 :display-function 'display-application
+	 :scrollbars t)
+   (inter :interactor :width 800 :height 100))
+  (:layouts
+   (:default (vertically () text inter))))
